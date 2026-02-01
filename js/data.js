9710304082ce9c659ac08c71ffa125a2b078d911
lethaison <son@gmail.com>
2026-02-01 15:07:32 +0700

    data js

---
+++ js/data.js
@@ -0,0 +1,20 @@
+const products = [
+    {
+        id: 1,
+        name: "iPhone 15 Pro",
+        price: "28.000.000đ",
+        image: "https://via.placeholder.com/200"
+    },
+    {
+        id: 2,
+        name: "Samsung S24 Ultra",
+        price: "25.500.000đ",
+        image: "https://via.placeholder.com/200"
+    },
+    {
+        id: 3,
+        name: "MacBook Air M3",
+        price: "32.000.000đ",
+        image: "https://via.placeholder.com/200"
+    }
+];
